cb5a47e1b911cc44a6f059c6bba5cbe7721a1f8e
Henry Van Styn <vanstyn@intellitree.com>
2016-12-11 19:25:10 -0500

    Placeholder template configs for dev
    Dummy/placeholder template configs and blank classes Access, and Store,
    which is new and under development (within RapidApp)

---
+++ lib/Rapi/Blog/Template/Store.pm
@@ -0,0 +1,13 @@
+package Rapi::Blog::Template::Store;
+use strict;
+use warnings;
+
+use RapidApp::Util qw(:all);
+
+use Moo;
+extends 'RapidApp::Template::Store';
+use Types::Standard ':all';
+
+
+
+1;
