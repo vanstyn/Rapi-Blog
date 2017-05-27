aa15d898085a3f924ae006aacc33b6e5db83fb1f
Henry Van Styn <vanstyn@intellitree.com>
2017-05-27 18:42:56 -0400

    ** list_posts API NOW WITH FULL PAGING FEATURES
    Greatly expanded the 'list_posts' API to support full suite of paging.
    It also now returns a data packet with lots of useful metadata about the
    paging info. It returns way more info than a consumer template would
    actually need (since much of it calculatable using only a smaller set of
    data points) but it returns all of these data points so its easier to
    write templates

--- lib/Rapi/Blog/DB/ResultSet/Post.pm
+++ lib/Rapi/Blog/DB/ResultSet/Post.pm
@@ -6,6 +6,8 @@ use warnings;
 use Moo;
 extends 'DBIx::Class::ResultSet';
 
+use RapidApp::Util ':all';
+
 sub published {
   (shift)
     ->search_rs({ 'me.published' => 1 })
@@ -39,14 +41,22 @@ sub _all_columns_except {
 
 # Method exposed to templates:
 sub list_posts {
-  my ($self, $search, $tag) = @_;
+  my ($self, $search, $tag, $page, $limit) = @_;
   
-  # TODO: define some sort of simple query API
+  # support alternate hashref named key/val argument
+  if(ref($search) && ref($search) eq 'HASH') {
+    my $h = $search;
+    ($search, $tag, $page, $limit) = ($h->{search}, $h->{tag}, $h->{page}, $h->{limit});
+  }
   
+  $limit = 500 unless ($limit && $limit =~ /^\d+$/);
+  $page  = 1   unless ($page  && $page  =~ /^\d+$/);
+
   my $Rs = $self
     ->published
     ->newest_first
-    ->_all_columns_except('body');
+    ->_all_columns_except('body')
+    ->search_rs();
   
   # -- example of how a query could work --
   $Rs = $Rs->search_rs(
@@ -59,7 +69,65 @@ sub list_posts {
     { join => 'post_tags' }
   ) if ($tag);
   
-  return [ $Rs->all ]
+  my @rows = ();
+  my $pages = 1;
+
+  my $total = $Rs->count;
+  if($total > 0) {
+    my $pages = int($total/$limit);
+    $pages++ if ($total % $limit);
+  }
+  
+  $page  = $pages if ($page > $pages);
+  
+  @rows = $Rs
+    ->search_rs(undef,{ page => $page, rows => $limit })
+    ->all if ($total > 0);
+    
+  my $count = (scalar @rows);
+  
+  my $thru = $page == 1 ? $count : ($page-1) * $limit + $count;
+  my $remaining = $total - $thru;
+  
+  my $last_page = $page == $pages ? 1 : 0;
+  
+  return {
+    # ArrayRef of Posts (this page)
+    rows      => \@rows,
+    
+    # Number of Posts returned (this page)
+    count     => $count,
+    
+    # Total number of posts (all pages)
+    total     => $total,
+    
+    # Page number of current page
+    page      => $page,
+    
+    # Total number of pages
+    pages     => $pages,
+    
+    # True is the current page is the last page
+    last_page => $page == $pages ? 1 : 0,
+    
+    # True if this page already contains all Posts
+    complete  => $total == $count ? 1 : 0,
+    
+    # The number (out of total Posts) this page starts at
+    start     => $thru - $count + 1,
+    
+    # The number (out of total Posts) this page ends at
+    end       => $thru,
+    
+    # The number of Posts remaining after this page
+    remaining => $remaining,
+    
+    # The number of Posts in all the pages before this one
+    before    => $thru - $count,
+    
+    # The limit of Posts per page
+    limit     => $limit
+  }
 }
 
 
