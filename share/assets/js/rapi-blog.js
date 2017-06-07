3a199f76078b9997177ac14c92f9a38a52cca80d
Henry Van Styn <vanstyn@intellitree.com>
2017-06-07 15:15:38 -0400

    postview: working general-function tab switcher WIP
    Not yet done any styling/design, but there is now a general-function tab
    switcher implementation which currently allows switching between
    "preview" and "source"

--- share/assets/js/rapi-blog.js
+++ share/assets/js/rapi-blog.js
@@ -1,21 +1,36 @@
 
-function rablActivateTab(event) {
-  var tab1 = event.target.nextElementSibling;
-  var tab2 = tab1.nextElementSibling;
-  
-  if(tab1.style.display == 'none') {
-    tab2.style.display = 'none';
-    tab1.style.display = 'block';
-  }
-  else if(tab2.style.display == 'none') {
-  
-    var iframe = tab2.getElementsByTagName('iframe')[0];
-    if(iframe) {
-      iframe.src = iframe.src;
-    }
-  
-    tab1.style.display = 'none';
-    tab2.style.display = 'block';
-  }
+function rablActivateTab(event,name) {
 
+  var fn;
+  fn = function(node) {
+    if(!node) { return null; }
+    return node.classList.contains('rapi-blog-postview')
+      ? node
+      : fn(node.parentElement);
+  };
+  
+  var topEl = fn(event.target);
+  if(!topEl) { return false; }
+  
+  var links = topEl.getElementsByClassName('tab-link');
+  var conts = topEl.getElementsByClassName('tab-content');
+  
+  for (i = 0; i < links.length; i++) {
+    var el = links[i];
+    el.classList.remove('active');
+    if(el.classList.contains(name)) {
+      el.classList.add('active');
+    }
+  }
+  
+  for (i = 0; i < conts.length; i++) {
+    var el = conts[i];
+    if(el.classList.contains(name)) {
+      el.style.display = 'block';
+    }
+    else {
+      el.style.display = 'none';
+    }
+  }
+  
 }
