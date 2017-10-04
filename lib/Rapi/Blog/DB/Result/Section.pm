344c5090aef19061cd4226ad9c805d123ae33e75
Henry Van Styn <vanstyn@intellitree.com>
2017-10-04 14:39:06 -0400

    Sections: prevent circular refs, max depth (10) and permissions
    Added biz logic to prevent making a section of child of itself, and also
    enforce a max depth of sections, currently 10 which is probably already
    excessive.
    
    Also added perm checks to only allow admins to create, modify or delete
    sections

--- lib/Rapi/Blog/DB/Result/Section.pm
+++ lib/Rapi/Blog/DB/Result/Section.pm
@@ -63,6 +63,71 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07045 @ 2017-10-03 18:30:46
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:c1GdRBNIYNKFc5wrrIdVcg
 
+use RapidApp::Util ':all';
+use Rapi::Blog::Util;
+
+__PACKAGE__->load_components('+Rapi::Blog::DB::Component::SafeResult');
+
+sub insert {
+  my $self = shift;
+  my $columns = shift;
+  
+  if(my $User = Rapi::Blog::Util->get_User) {
+    die usererr "Create Section: PERMISSION DENIED" if ($User->id && !$User->admin);
+  }
+  
+  $self->set_inflated_columns($columns) if $columns;
+  
+  $self->_validate_depth;
+  
+  $self->next::method
+}
+
+sub update {
+  my $self = shift;
+  my $columns = shift;
+  
+  if(my $User = Rapi::Blog::Util->get_User) {
+    die usererr "Update Section: PERMISSION DENIED" if ($User->id && !$User->admin);
+  }
+  
+  $self->set_inflated_columns($columns) if $columns;
+  
+  $self->_validate_depth;
+  
+  $self->next::method
+}
+
+
+sub delete {
+  my $self = shift;
+  
+  if(my $User = Rapi::Blog::Util->get_User) {
+    die usererr "Delete Section: PERMISSION DENIED" if ($User->id && !$User->admin);
+  }
+  
+  $self->next::method(@_)
+}
+
+sub _validate_depth {
+  my $self = shift;
+  my $level = shift || 1;
+  my $seen = shift || {};
+  
+  my $max_depth = 10;
+  
+  if (my $id = $self->get_column('id')) { # may not have an id yet
+    die usererr "Circular references not allowed (cannot make a Section a child of itself)" if ($seen->{$id}++);
+  }
+  
+  die usererr "Too many levels of Sections -- max allowed is $max_depth" if ($level > $max_depth);
+  
+  $self->get_column('parent_id')
+    ? $self->parent->_validate_depth($level+1,$seen)
+    : 1
+}
+
+
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
